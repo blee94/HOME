16c692ef86165a67bbd73a89da4390edd6e439f2
blee94 <135qudwls@naver.com>
2023-10-11 01:00:23 +0900

    node.js

---
+++ node.js/index.js
@@ -0,0 +1,6 @@
+const { add, minus, mult, divide } = require("./math");
+
+console.log("더하기", add(3, 4));
+console.log("빼기", minus(9, 4));
+console.log("곱하기", mult(3, 7));
+console.log("나누기", divide(9, 3));
---
+++ node.js/math.js
@@ -0,0 +1,19 @@
+function add(n1, n2) {
+    return n1 + n2;
+}
+function minus(n1, n2) {
+    return n1 - n2;
+}
+function mult(n1, n2) {
+    return n1 * n2;
+}
+function divide(n1, n2) {
+    return n1 / n2;
+}
+
+module.exports = {
+    add,
+    minus,
+    mult,
+    divide,
+};
