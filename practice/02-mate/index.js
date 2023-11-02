31b7dc40cd3e9a85dd947186fb0d3274a58d718b
blee94 <135qudwls@naver.com>
2023-11-03 03:03:59 +0900

    mate 2

---
+++ practice/02-mate/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const PORT = 8000;
+
+app.set('view engine', 'ejs');
+app.use('/static', express.static('static'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const router = require('./routes');
+app.use('/', router);
+
+app.listen(PORT, function () {
+  console.log(`Sever Open: ${PORT}`);
+});
